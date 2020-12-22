9d493cff2b633ccb79e3d34cb47e4a36c85dbd88
MacBook <macbook@zaidanrsy.local>
2020-12-22 16:55:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/instastep/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.instastep
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), SensorEventListener{
+
+    private var sensorManager: SensorManager? = null
+
+    private var running = false
+    private var totalSteps = 0f
+    private var previousTotalSteps = 0f
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loadData()
+        resetSteps()
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onResume() {
+        super.onResume()
+        running = true
+        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if (stepSensor == null) {
+            Toast.makeText(this,"No sensor detected on this device",Toast.LENGTH_SHORT).show()
+        }else{
+            sensorManager?.registerListener(this,stepSensor,SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (running){
+            totalSteps = event!!.values[0]
+            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
+            tv_stepsTaken.text = ("$currentSteps")
+
+            progress_circular.apply {
+                setProgressWithAnimation(currentSteps.toFloat())
+            }
+        }
+    }
+
+    private fun resetSteps() {
+        tv_stepsTaken.setOnClickListener {
+            Toast.makeText(this,"Long tap reset steps", Toast.LENGTH_SHORT).show()
+        }
+
+        tv_stepsTaken.setOnLongClickListener{
+            previousTotalSteps = totalSteps
+            tv_stepsTaken.text = 0.toString()
+            saveData()
+
+            true
+        }
+    }
+    private fun saveData() {
+        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putFloat("key", previousTotalSteps)
+        editor.apply()
+    }
+    private fun loadData() {
+        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val savedNumber = sharedPreferences.getFloat("key1", 0f)
+        Log.d("MainActivity", "$savedNumber")
+        previousTotalSteps = savedNumber
+    }
+}
